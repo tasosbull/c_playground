6db05f3b63dc01829baff2666954b411f250f1cd
tasosbull <tasosbull@gmail.com>
2021-07-29 16:44:18 +0300

    .

--- src/test/core_hello.c
+++ src/test/core_hello.c
@@ -8,7 +8,7 @@ int core_hello()
     core* m_core = core_new();
 
     mapAdd( "test1", "hello", m_core->env);
-    char* str = (char*)mapGet("test1", m_core->env);
+    const char* str = (const char*)mapGet("test1", m_core->env);
     printf("env param test1:%s\n", str);
     core_free(m_core);
     return 0;
