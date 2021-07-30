52f7e26b9919ee5acd2f8543e718c5dfc35bbac0
tasosbull <tasosbull@gmail.com>
2021-07-30 17:57:44 +0300

    .

--- src/main.c
+++ src/main.c
@@ -1,11 +1,9 @@
-#include "test/gtk_hello.h"
 #include "test/sqlite_hello.h"
 #include "test/ini_hello.h"
 #include "test/log_hello.h"
 #include "test/map_hello.h"
 #include "test/core_hello.h"
 
-static int b_gtk_hello = 0;
 static int b_sqlite_hello = 0;
 static int b_sqlite_hello_create = 0;
 static int b_sqlite_hello_update = 0;
@@ -17,9 +15,7 @@ static int b_core_hello = 1;
 
 int main (int argc, char **argv)
 {  
-   if(b_gtk_hello)
-      return gtk_hello(argc, argv);
-   else if(b_sqlite_hello)
+   if(b_sqlite_hello)
       return sqlite_hello(b_sqlite_hello_create, b_sqlite_hello_update);
    else if(b_ini_hello)
       return ini_hello();
